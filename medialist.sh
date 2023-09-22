6f671a973d4cb5b7c4f4eae50e5c6e0620a01fe6
Jason Ort <jorts.tech@gmail.com>
2023-09-22 11:52:36 -0400

    updating medialist to version with global variables, for portability to outside users

--- medialist.sh
+++ medialist.sh
@@ -4,91 +4,65 @@
 	# note - Each function avialable as an existing standalone script in /home/bilbo/Github_Repositories/bash-scripts/
 # Author - jort
 
-#Global variable for log data
-declare timestamp=$(date)
+#-----------------------------------------------------------------
+#Global Variables:
+#User-added libraries go here (please replace filepath with master folder for each type of media)
+declare -g TVsearch="/jortpool/LabStorage/Media/TV"
+declare -g moviesearch="/jortpool/LabStorage/Media/Movies"
+declare -g gamesearch="/jortpool/LabStorage/Media/Games/"
+#Log-related variables here. User only needs to update for their desired log path
+declare -g LogPath="/jortpool/LabStorage/Media/Lists"
+	declare -g rawTV="$LogPath/.rawTV.txt"
+	declare -g completedTV="$LogPath/TV_Library.txt"
+	declare -g completedMovies="$LogPath/Film_Library.txt"
+	declare -g completedgames="$LogPath/Game_Library.txt"
+	declare -g mkvlist="$LogPath/.mkvlist.txt"
+	declare -g mp4list="$LogPath/.mp4list.txt"
+	declare -g rawgames="$LogPath/.rawlistgames.txt"
+#---------------------------------------------------------------
+
 function TVlist {
-# VARIABLES ------------------------------------------------------
+	touch $rawTV $completedTV
+	ls $TVsearch > $rawTV
 
-#designated directory to search
-TVsearch="/jortpool/LabStorage/Media/TV"
+	echo "Full List of TV Shows available on this system: " > $completedTV #Overwrites old data
+	echo "---------------------------------------------" >> $completedTV
 
-#name of raw search restult log files
-rawTV="/jortpool/LabStorage/Media/Lists/.rawTV.txt"
-
-#name of final result logs
-completedTV="/jortpool/LabStorage/Media/Lists/TV_Library.txt"
-
-# SCRIPT PROPER -------------------------------------------------
-touch $rawTV $completedTV
-ls $TVsearch > $rawTV
-
-echo "Data current as of: $timestamp" > $completedTV
-echo "---------------------------------------------" >> $completedTV
-echo "Full List of TV Shows available on this system: " >> $completedTV
-echo "---------------------------------------------" >> $completedTV
-
-sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Movies/-/;' $rawTV >>  $completedTV
+	sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Movies/-/;' $rawTV >>  $completedTV
 
 }
 
 function movielist {
-# VARIABLES ------------------------------------------------------
+	#tasks
+	findmkv="find $moviesearch -path $moviesearch*.mkv"
+	findmp4="find $moviesearch -path $moviesearch*.mp4"
 
-#designated directory to search
-moviesearch="/jortpool/LabStorage/Media/Movies"
+	# SCRIPT PROPER -------------------------------------------------
+	clear
+	touch $mkvlist $mp4list
+	$findmkv > $mkvlist
+	$findmp4 > $mp4list
+	echo "Full List of flims available on this system: " > $completedMovies #Overwrites old data
+	echo "---------------------------------------------" >> $completedMovies
 
-#name of raw search restult log files
-mkvlist="/jortpool/LabStorage/Media/Lists/.mkvlist.txt"
-mp4list="/jortpool/LabStorage/Media/Lists/.mp4list.txt"
-
-#name of final result logs
-completedMovies="/jortpool/LabStorage/Media/Lists/Film_Library.txt"
-
-#tasks
-findmkv="find $moviesearch -path $moviesearch*.mkv"
-findmp4="find $moviesearch -path $moviesearch*.mp4"
-
-# SCRIPT PROPER -------------------------------------------------
-clear
-touch $mkvlist $mp4list
-$findmkv > $mkvlist
-$findmp4 > $mp4list
-
-echo "Data current as of: $timestamp" > $completedMovies
-echo "---------------------------------------------" >> $completedMovies
-echo "Full List of flims available on this system: " >> $completedMovies
-echo "---------------------------------------------" >> $completedMovies
-
-sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Movies/-/;' $mkvlist >>  $completedMovies
-sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Movies/-/;' $mp4list >>  $completedMovies
+	sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Movies/-/;' $mkvlist >>  $completedMovies
+	sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Movies/-/;' $mp4list >>  $completedMovies
 
 }
 
 function gamelist {
-#designated directory to search
-gamesearch="/jortpool/LabStorage/Media/Games/"
+	touch $rawgames $completedgames
+	tree $gamesearch > $rawgames
 
-#name of raw search restult log files
-rawgames="/jortpool/LabStorage/Media/Lists/.rawlistgames.txt"
+	echo "Full List of game ROMs available on this system: " > $completedgames #Overwrites old data
+	echo "---------------------------------------------" >> $completedgames
 
-
-#name of final result logs
-completedgames="/jortpool/LabStorage/Media/Lists/Game_Library.txt"
-touch $rawgames $completedgames
-
-echo "searching..."
-
-tree $gamesearch > $rawgames
-
-echo "Data current as of: $timestamp" > $completedgames
-echo "---------------------------------------------" >> $completedgames
-echo "Full List of game ROMs available on this system: " >> $completedgames
-echo "---------------------------------------------" >> $completedgames
-
-sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Games/-/;' $rawgames >>  $completedgames
+	sed -e 's/jortpool/-/; s/LabStorage/-/; s/Media/-/; s/Games/-/;' $rawgames >>  $completedgames
 }
 
 TVlist
 movielist
 gamelist
-exit 0
+
+cls
+echo "it worked! do you want a cookie or something?"
