8af4390ee0a8995ae4dab3e9077df84d5d1036a6
Jason Ort <jorts.tech@gmail.com>
2024-04-08 18:22:26 -0400

    properly titled production copy

--- sensorlog.sh
+++ sensorlog.sh
@@ -1,33 +1,72 @@
 #!/bin/bash
 
-touch /home/bilbo/SensorLogs.txt
-declare log="/home/bilbo/SensorLogs.txt"
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#SensorLogs - Logging & Audit Script
+#Author - JORT
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#Set refresh time in seconds (default == 5s)
+declare refresh_timer=5
+
+#Set desired location for log file
+declare log="/home/bilbo/SensorLogs.txt"
+ touch $log
+
+#Full Logging Script:
 while true; do
-  date > $log
+  date > $log #ensures overwrite previous log entries
+
+#Basic system information
   echo "---------------------" >> $log
-  echo " "
+  echo "System Information:" >> $log
+  echo "---------------------" >> $log
+	echo "Hostname: $(hostname)" >> $log
+	echo "Linux Version: $(uname -r)" >> $log
+	echo "System Uptime: $(uptime -p)" >> $log
+	  echo "~~~~~~~~~~~~~~~~" >> $log
+	  echo "users logged in::" >>$log
+	  echo "~~~~~~~~~~~~~~~~" >> $log
+	  	who >> $log
+echo " " >>$log
+
+#Thermal Logs (requires lmsensors - install & run to configure sensors before including this section)
+#Uncomment to enable
   echo "================" >>$log
   echo "Thermal Sensors:" >>$log
   echo "================" >>$log
-  echo " "
+echo " "  >>$log
 	  sensors >> $log
+
+#Network Check
   echo "----------------------------" >>$log
   echo "   Network Connectivity:" >>$log
   echo "----------------------------" >>$log
 	ifconfig >>$log
- echo " "
-  echo "----------------------------" >> $log
-  echo "*****ACTIVE CONNECTIONS*****:" >>$log
-  echo "----------------------------" >>$log
+echo " " >>$log
+  echo "*******************" >> $log
+  echo "ACTIVE CONNECTIONS:" >>$log
+  echo "*******************" >> $log
 	sudo netstat -pano | grep ESTABLISHED | awk '{print $4, $5, $7, $8, $9}' > /tmp/ports.list ; sed -i 's/ /....../g' /tmp/ports.list && cat /tmp/ports.list >> $log
- echo " "
+echo " " >>$log
+
+#Check Filesystems
   echo "----------------------------" >> $log
   echo " Storage Capacity Detected:" >>$log
   echo "----------------------------" >>$log
 	  df -hT >>$log
- echo " "
- echo "List of new files added to server can be found here: /jortpool/LabStorage/Personal_Vault/Nightly-Backup-Log.txt" >> $log
-	sleep 5
+
+echo " " >>$log
+
+#Add any Custom Messages here:
+echo "****************" >>$log
+echo "Custom Messages:" >>$log
+echo "****************" >>$log
+
+echo "List of new files added to server can be found here: /jortpool/LabStorage/Personal_Vault/Nightly-Backup-Log.txt" >> $log
+echo "Thank you for running SensorLogs -JORT" >> $log
+
+
+#Refresh log
+sleep $refresh_timer
 done
 
